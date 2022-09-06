32d54177cba846263f697b08d90a4012bd13e812
Anastasia <nastyaerm@mail.ru>
2022-09-06 17:08:22 +0200

    update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     menu.classList.remove('active')
 })
+
+const counters = document.querySelectorAll('.skills__rating-counter')
+      lines = document.querySelectorAll('.skills__rating-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
