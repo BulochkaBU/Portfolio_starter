2ad52ce27d8ff32275572cb7f8aada376264bb8d
Anastasia <nastyaerm@mail.ru>
2022-09-08 21:01:51 +0200

    create section Contacts, policy, updated sidepanel

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,36 @@ const counters = document.querySelectorAll('.skills__rating-counter')
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 })
+
+const sidepanel = document.querySelector('.sidepanel')
+const divider = document.querySelector('.sidepanel__divider')
+const link = document.querySelectorAll('.sidepanel__link svg path')
+
+window.addEventListener("scroll", function() {
+    if (document.documentElement.scrollTop >= 250) {    
+        sidepanel.style.color = '#0f8853';
+        divider.style.background = '#0f8853';
+        link.forEach(item => {
+            item.style.fill = '#0f8853'
+            item.addEventListener('mouseenter', function() {
+                item.style.fill = '#324265'
+            })
+            item.addEventListener('mouseleave', function() {
+                item.style.fill = '#0f8853'
+            })
+        })
+    } else {        
+        sidepanel.style.color = '#fff';
+        divider.style.background = '#fff';
+        link.forEach(item => {
+            item.style.fill = '#fff'
+            item.addEventListener('mouseenter', function() {
+                item.style.fill = '#0f8853'
+            })
+            item.addEventListener('mouseleave', function() {
+                item.style.fill = '#fff'
+            })
+        })
+    }
+})
+
